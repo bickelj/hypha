3e31d00bd061c810c6e5f04e12b0a64ef73d971a
Ghulam Murtaza <gmurtaza@opentechstrategies.com>
2022-11-23 12:22:39 -0500

    Make reviews box collapsible
    Default to collapsed.
    
    Issue #3975

---
+++ hypha/static_src/javascript/toggle-reviewers.js
@@ -0,0 +1,34 @@
+(function ($) {
+    "use strict";
+
+    // get all the reviewers that are missing
+    const reviewers = Array.prototype.slice.call(
+        $(".js-reviews-sidebar").find(".is-hidden.no-response")
+    );
+
+    $(".js-toggle-reviewers").click(function (e) {
+        e.preventDefault();
+
+        // toggle class and update text
+        $(this).toggleClass("is-open");
+        if ($(this).hasClass("is-open")) {
+            $(this).html("Hide All Assigned Reviewers");
+        } else {
+            $(this).html("All Assigned Reviewers");
+        }
+
+        // toggle the reviewers
+        toggleReviewers(reviewers);
+    });
+
+    // show/hide the reviewers
+    function toggleReviewers(reviewers) {
+        reviewers.forEach((reviewer) => {
+            $(reviewer).toggleClass("is-hidden");
+        });
+    }
+    $(".sidebar__inner--heading > .arrow").click(function (e) {
+        $(this).parent().parent().toggleClass("is-expanded");
+        $(".reviews-sidebar").toggleClass("is-hidden");
+    });
+})(jQuery);
